16e0cae95959e5fc635313a9a6a2e84745cf134a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-27 22:35:18 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,8 @@ import "./styles.css";
 export default function App() {
   const [someData, setSomeData] = ([]);
 
-  function submitForm(e) {
-    setSomeData(e.target.value);
-    console.log("red");
+  function submitForm() {
+    console.log(someData);
   }
 
   const changeData = (e) => {
@@ -16,7 +15,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <form onSubmit={submitForm}>
-        <input type="text" value={someData} />
+        <input type="text" onChange={changeData} value={someData} />
         <input type="submit" value="submit" />
       </form>
     </div>
